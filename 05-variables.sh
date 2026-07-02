ec5dfe651ceb959737a20919d8e3e15be695237c
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-02 08:35:58 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please enter your PIN number"
+read -s PIN_NUMBER #here PIN_NUMBER is variable and -s is to hide the password which we enter in the server
+
+echo "PIN entered is $PIN_NUMBER"
