efb1afde480a1d989bc236e2052c26f851100016
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-04 09:53:57 +0530

    colors

---
+++ 16-logs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Sccript started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root previllege"
+    exit 1 #failure is other than 0
+
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "Installing $2... $G SUCCESS $N" | tee -a $LOG_FILE
+
+    fi
+}
+
+dnf list installed mysql
+#install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MySQL"
+
+else
+    echo -e "MYSQL already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
+
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &??$LOG_FILE
+    VALIDATE $? "Nginx"
+
+else
+    echo -e "Nginx already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
+
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "Python3"
+
+else
+    echo -e "Python3 already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
+
+fi
